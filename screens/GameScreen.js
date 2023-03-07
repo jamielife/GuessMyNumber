ab1fb95a322f39affcced94623d96b230f07f912
Jamie Taylor <jvptaylor@hotmail.com>
2023-03-06 23:29:25 -0500

    created Title component, added styling, used StatusBar for height in place of SaveView

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,28 +1,30 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, StatusBar } from "react-native";
+import Title from "../components/Title";
 
 function GameScreen(){
-    return <View style={styles.screen}>
-        <Text>Opponent's Guess</Text>
-        {/* Guess */}
-        <View>
-            <Text>Higher or lower?</Text>
-            {/* + - */}
+    return (
+        <View style={styles.screen}>
+            <Title>Opponent's Guess</Title> 
+            {/* Guess */}
+            <View>
+                <Text>Higher or lower?</Text>
+                {/* + - */}
+            </View>
+            <View>
+                {/* Log Rounds */}
+            </View>
         </View>
-        <View>
-            {/* Log Rounds */}
-        </View>
-    </View>
-
-
-
-
+    );
 }
+
+console.log(Platform.OS);
 
 export default GameScreen;
 
 const styles = StyleSheet.create({
         screen: {
             flex: 1,
-            padding: 12,
-        },
+            padding: 24,            
+            paddingTop: StatusBar.currentHeight + 10,
+        }
 })
